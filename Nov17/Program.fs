3c48aebbc63bbcbb50217cbd26e69fa082e7205b
Tom Rowsell <tomrowsell@hod165887.local>
2022-11-23 17:04:32 +0000

    fix to algorithm

--- Nov17/Program.fs
+++ Nov17/Program.fs
@@ -8,7 +8,20 @@ type NaughtOrNiceCheck =
 let lines = System.IO.File.ReadLines("data.txt")
 let vowels = [ 'a'; 'e'; 'i'; 'o'; 'u' ]
 let forbiddenStrings = [ "ab"; "cd"; "pq"; "xy" ]
-let forbiddenStringCheck input = List.contains input forbiddenStrings
+
+let forbiddenStringCheck (input:string) =
+    let rec inner (input:list<char>) =
+        match input with
+        | a :: b :: tl  ->
+            let concatExp = System.String.Concat(a, b)
+            if (List.contains concatExp forbiddenStrings) then
+                true
+            else
+                inner (b :: tl)
+        | [ _ ]
+        | [] -> false
+    inner (List.ofArray(input.ToCharArray()))
+
 let vowelCheck c = List.contains c vowels
 
 let vowelCountMapper (input: string) =
@@ -29,12 +42,10 @@ let filteredSet =
     Seq.map
         (fun x ->
             { Expression = x
-              VowelCheck = ((vowelCountMapper x) > 3)
+              VowelCheck = ((vowelCountMapper x) > 2)
               DoubleCheck = (doubleLetterCheck x)
               ForbiddenLetterCheck = (forbiddenStringCheck x) })
         lines
-
-printfn "Lines found %d" (Seq.length lines)
 
 let result =
     Seq.where (fun x -> x.VowelCheck && not (x.ForbiddenLetterCheck) && x.DoubleCheck) filteredSet
